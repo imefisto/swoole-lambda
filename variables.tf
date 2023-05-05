2e13fb4d5cd9d0f1bd371c3aa9e52e9a9012b3be
Ignacio El Kadre <imefisto@gmail.com>
2023-05-05 07:19:30 -0300

    chore: put the region as variable

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "region" {
+  description = "Set your region"
+  type = string
+  default = "us-east-1"
+}
+
 variable "urls" {
   description = "List of urls to send a GET request"
   type = list
